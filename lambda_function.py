0a3c494dfd1cdb36dd4e338d1f99feb2043bb41b
Adrian <adrian.dev@kousz.ch>
2020-12-06 11:17:30 +0100

    Add binary type handling

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,4 @@
+from base64 import b64encode as base64
 from json import dumps as jsonify
 from os.path import isfile as isfile
 from util import getConfig, getMails
@@ -45,14 +46,19 @@ def messages(config, event):
 
 def render(config, path):
 
-	result = html('Not found', 404)
+	result = text('Not found', 'text/plain', 404)
 	path = config['assetpath'] + (config['paths']['index'] if path == '/' else path)
-	
-	type = [ type for (ext, type) in config['types'].items() if path.endswith(ext) ]
+
+	type = [ t for (ext, t) in config['types'].items() if path.endswith(ext) ][0]
+	isbin = [ isbin for (t, isbin) in config['binary_types'].items() if type.startswith(t) ][0]
 
 	if isfile(path):
-		with open(path) as file:
-			result = html(file.read(), type[0])
+		if isbin:
+			with open(path, 'rb') as file:
+				result = binary(file.read(), type)
+		else:
+			with open(path) as file:
+				result = text(file.read(), type)
 
 	return result
 
@@ -61,9 +67,18 @@ def json(content: dict, status: int = 200):
 	return { 'statusCode': status, 'body': jsonify(content) }
 
 
-def html(content: str, type: str, status: int = 200):
+def text(content: str, type: str, status: int = 200):
 	return {
 		'statusCode': status,
+		'headers': { 'Content-Type': type, 'Cache-Control': 'max-age=86400' },
 		'body': content,
-		'headers': { 'Content-Type': type, 'Cache-Control': 'max-age=864000' }
+	}
+
+
+def binary(content: bytes, type: str):
+	return {
+		'statusCode': 200,
+		'isBase64Encoded': True,
+		'headers': { 'Content-Type': type, 'Cache-Control': 'max-age=86400' },
+		'body': base64(content).decode("utf-8"),
 	}
